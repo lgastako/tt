82c2e5dd967292f793965dba7680e289d801f2da
John Evans <lgastako@gmail.com>
2023-07-15 21:39:00 -0400

    "Added Template functionality to TT module and included row-types dependency"
    Model used: gpt-4-0613
    
    me> Please create stubs for the functions describe in the README in src/TT.hs
    jr>  I 've  created  the  stub s  for  the  functions  described  in  the  README  in  the  file  ` src / TT .hs `.  The  functions  ` render `,  ` partial `,  and  ` my Template `  are  now  defined  with  their  respective  types ,  but  their  implementations  are  currently  undefined .  You  can  now  proceed  to  implement  these  functions  as  per  your  project  requirements .

--- src/TT.hs
+++ src/TT.hs
@@ -1,3 +1,18 @@
 module TT where
 
-import Data.Row
+import Data.Text
+
+-- | A template is a function from a row type to text.
+type Template a = a -> Text
+
+-- | Render the template by passing a complete row-type.
+render :: Template a -> a -> Text
+render = undefined
+
+-- | Partially apply a template by passing a partial row-type.
+partial :: Template a -> a -> Template a
+partial = undefined
+
+-- | Example template
+myTemplate :: Template ("name" .== Text .+ "age" .== Int)
+myTemplate = undefined
